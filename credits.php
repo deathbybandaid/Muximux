ab9d3d6e59167b616ea6760de6855cae84158885
deathbybandaid <something_duck@hotmail.com>
2016-11-06 08:46:24 -0500

    Add files via upload

---
+++ credits.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1"/>
+<title>PHP-Info</title>
+</style>
+<style type="text/css">
+<!--
+body{background-image: url(http://deathbybandaid.ddns.net/errorpages/landing-bg.jpg);}
+-->
+</style>
+</head>
+<body>
+<?php
+phpinfo(INFO_CREDITS);
+?>
+</body>
+</html>
---
+++ settings.ini.php
@@ -0,0 +1,84 @@
+; <?php die("Access denied"); ?>
+[general]
+title = "PHPInfo"
+branch = "master"
+enabledropdown = "true"
+
+[settings]
+enabled = "true"
+default = "false"
+name = "Settings"
+url = "muximux.php"
+landingpage = "false"
+icon = "fa-cog"
+dd = "true"
+
+[General]
+name = "General"
+url = "http://localhost/phpinfo/general.php"
+icon = ""
+enabled = "true"
+default = "true"
+landingpage = "false"
+dd = "false"
+scale = 1
+
+[Configuration]
+name = "Configuration"
+url = "http://localhost/phpinfo/configuration.php"
+icon = ""
+enabled = "true"
+default = "false"
+landingpage = "false"
+dd = "false"
+scale = 1
+
+[Modules]
+name = "Modules"
+url = "http://localhost/phpinfo/modules.php"
+icon = ""
+enabled = "true"
+default = "false"
+landingpage = "false"
+dd = "false"
+scale = 1
+
+[Environment]
+name = "Environment"
+url = "http://localhost/phpinfo/environment.php"
+icon = ""
+enabled = "true"
+default = "false"
+landingpage = "false"
+dd = "false"
+scale = 1
+
+[Variables]
+name = "Variables"
+url = "http://localhost/phpinfo/variables.php"
+icon = ""
+enabled = "true"
+default = "false"
+landingpage = "false"
+dd = "false"
+scale = 1
+
+[Credits]
+name = "Credits"
+url = "http://localhost/phpinfo/credits.php"
+icon = ""
+enabled = "true"
+default = "false"
+landingpage = "false"
+dd = "false"
+scale = 1
+
+[Licence]
+name = "Licence"
+url = "http://localhost/phpinfo/licence.php"
+icon = ""
+enabled = "true"
+default = "false"
+landingpage = "false"
+dd = "false"
+scale = 1
